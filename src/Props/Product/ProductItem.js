b28dfa1fc841e46975dc43d6c987ffbc4934ac92
thanhlong2023 <caolong770709@gmail.com>
2025-02-24 17:12:49 +0700

    truyền props qua nhiều component

--- src/Props/Product/ProductItem.js
+++ src/Props/Product/ProductItem.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react'
 
 export default class ProductItem extends Component {
     render() {
+        let { name, price, image } = this.props.dataProductItem
         return (
-            <div>ProductItem</div>
+            <div className='card text-left'>
+                <img style={{ width: '350px' }} src={image} alt={image} className='card-img-top' />
+                <div className="card-body">
+                    <h4 className='card-title'>{name}</h4>
+                    <p className='card-text'>{price}</p>
+                </div>
+            </div>
         )
     }
 }
--- src/Props/Product/ProductList.js
+++ src/Props/Product/ProductList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import ProductItem from './ProductItem';
 
 export default class ProductList extends Component {
 
+    renderProductItem = () => {
+        return this.props.arrProduct.map((item, index) => {
+            return (<div className='col-4 mt-3' key={index}>
+                <ProductItem dataProductItem={item} />
+            </div>)
+        })
+    }
     render() {
-        console.log(this.props);
+        // console.log(this.props);
         return (
-            <div>
-                product list
+            <div className='row'>
+                {this.renderProductItem()}
             </div>
         )
     }
